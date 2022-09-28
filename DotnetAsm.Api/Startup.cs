44519df04b05230c1c9ad5f46f2e566bdcb52216
Sergiusz Zalewski <twinker3228@gmail.com>
2022-09-28 19:29:17 +0200

    Added disasm summary ouptput to api response

--- DotnetAsm.Api/Startup.cs
+++ DotnetAsm.Api/Startup.cs
@@ -63,7 +63,6 @@ public class Startup
             {
                 spaBuilder.UseProxyToSpaDevelopmentServer(_configuration.GetValue<string>("FrontendUrl")!);
             }
-
         });
     }
 }
