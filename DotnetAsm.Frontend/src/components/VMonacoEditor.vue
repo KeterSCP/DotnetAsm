e86d995590d4e6f27e5b12a0489139ab4e2aae32
Sergiusz Zalewski <twinker3228@gmail.com>
2023-02-05 16:40:16 +0100

    Rewrite editors using monaco editor

--- DotnetAsm.Frontend/src/components/VMonacoEditor.vue
+++ DotnetAsm.Frontend/src/components/VMonacoEditor.vue
@@ -1,34 +1,84 @@
 <template>
-    <div id="editor" ref="editor" />
+    <div :id="id" />
 </template>
 
 <script setup lang="ts">
-import loader from "@monaco-editor/loader";
 import { editor } from "monaco-editor";
-import { onMounted, PropType, toRefs } from "vue";
+import { onMounted, PropType, toRefs, onBeforeUnmount, watch, ref } from "vue";
+import darkTheme from "src/theme/dark-theme";
 
 import IStandaloneEditorConstructionOptions = editor.IStandaloneEditorConstructionOptions;
 
 const props = defineProps({
+    modelValue: {
+        type: String,
+        required: true,
+    },
+    id: {
+        type: String,
+        required: true,
+    },
+    darkThemeName: {
+        type: String,
+        required: true,
+    },
+    lightThemeName: {
+        type: String,
+        required: true,
+    },
     options: {
         type: Object as PropType<IStandaloneEditorConstructionOptions>,
         required: true,
     },
 });
 
-const { options } = toRefs(props);
+const emits = defineEmits(["update:modelValue"]);
+const { options, modelValue, id, darkThemeName, lightThemeName } = toRefs(props);
+
+let internalEditor: editor.IStandaloneCodeEditor | null = null;
+let textModel: editor.ITextModel | null = null;
+
+const contentBackup = ref("");
+const isSettingContent = ref(false);
 
 onMounted(() => {
-    loader.init().then((monaco) => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        monaco.editor.create(document.getElementById("editor")!, options.value);
+    textModel = editor.createModel(modelValue.value, options.value.language);
+    textModel.onDidChangeContent(() => {
+        const val = textModel?.getValue();
+        contentBackup.value = val ?? "";
+        emits("update:modelValue", val);
+    });
+
+    contentBackup.value = textModel.getValue();
+
+    options.value.theme = darkTheme.value ? darkThemeName.value : lightThemeName.value;
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    internalEditor = editor.create(document.getElementById(id.value)!, options.value);
+
+    internalEditor.setModel(textModel);
+});
+
+onBeforeUnmount(() => {
+    internalEditor?.dispose();
+    internalEditor = null;
+});
+
+watch(modelValue, (newValue) => {
+    if (contentBackup.value !== newValue) {
+        try {
+            isSettingContent.value = true;
+            textModel?.setValue(newValue);
+        } finally {
+            isSettingContent.value = false;
+        }
+        contentBackup.value = newValue;
+    }
+});
+
+watch(darkTheme, (dark) => {
+    internalEditor?.updateOptions({
+        theme: dark ? darkThemeName.value : lightThemeName.value,
     });
 });
 </script>
-
-<style scoped>
-#editor {
-    width: 100vw;
-    height: 100vh;
-}
-</style>
